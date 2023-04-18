1876df2986581f8ab8bce63009c8b9c86c113700
rlal2022 <lrahul2010@gmail.com>
2023-04-17 23:48:16 -0700

    test again

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -5,11 +5,11 @@ function find() {
 }
 
 function findById(id) {
-  return db("users").where("id", id);
+  return db("users").where("id", id).first();
 }
 
-async function add(user) {
-  const [id] = await db("users").insert(user);
+async function add({ username, password }) {
+  const [id] = await db("users").insert({ username, password });
   return findById(id);
 }
 

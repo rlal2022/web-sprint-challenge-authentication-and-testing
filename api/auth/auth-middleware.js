365d1ed1388d7d939cd12a589a83a9d192da117e
rlal2022 <lrahul2010@gmail.com>
2023-04-18 01:28:50 -0700

    commit again..

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,45 +1,46 @@
 const User = require("../users/users-model");
 
 async function checkUsername(req, res, next) {
-  // try {
-  //   const { username } = req.body;
-  //   const [user] = await User.findBy({ username: username });
-  //   if (user) {
-  //     next({ status: 401, message: "username taken" });
-  //   } else {
-  //     next();
-  //   }
-  // } catch (err) {
-  //   next(err);
-  // }
-
-  const { username } = req.body;
-  const [user] = await User.findBy({ username: username });
-  if (user) {
-    next();
-  } else {
-    next({ status: 422, message: "username taken" });
+  try {
+    const { username } = req.body;
+    const [user] = await User.findBy({ username: username });
+    if (user) {
+      next({ status: 401, message: "username taken" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
   }
+
+  // const { username } = req.body;
+  // const [user] = await User.findBy({ username: username });
+  // if (user) {
+  //   next();
+  // } else {
+  //   next({ status: 422, message: "username taken" });
+  // }
 }
 
 async function checkIfUsernameExists(req, res, next) {
-  // const { username } = req.body;
-  // const [user] = await User.findBy({ username: username });
-  // if (!user) {
-  //   next({ status: 401, message: "Invalid credentials" });
-  // } else {
-  //   req.user = user;
-  //   next();
-  // }
-
   const { username } = req.body;
   const [user] = await User.findBy({ username: username });
-  if (user) {
-    next();
+  if (!user) {
+    next({ status: 401, message: "Invalid credentials" });
   } else {
-    next({ status: 401, message: "invalid credentials" });
+    req.user = user;
+    next();
   }
 }
+
+//   const { username } = req.body;
+//   const [user] = await User.findBy({ username: username });
+//   if (user) {
+//     next();
+//   } else {
+//     next({ status: 401, message: "invalid credentials" });
+//   }
+// }
 
 function checkCredentials(req, res, next) {
   const { username, password } = req.body;

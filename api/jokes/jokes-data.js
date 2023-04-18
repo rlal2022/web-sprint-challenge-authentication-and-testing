01a57de5f7078a060aa022b9572bd6d14fcefb84
rlal2022 <lrahul2010@gmail.com>
2023-04-17 21:11:56 -0700

    test

--- api/jokes/jokes-data.js
+++ api/jokes/jokes-data.js
@@ -1,16 +1,16 @@
 // do not make changes to this file
 const jokes = [
   {
-    "id": "0189hNRf2g",
-    "joke": "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later."
+    id: "0189hNRf2g",
+    joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later.",
   },
   {
-    "id": "08EQZ8EQukb",
-    "joke": "Did you hear about the guy whose whole left side was cut off? He's all right now."
+    id: "08EQZ8EQukb",
+    joke: "Did you hear about the guy whose whole left side was cut off? He's all right now.",
   },
   {
-    "id": "08xHQCdx5Ed",
-    "joke": "Why didn’t the skeleton cross the road? Because he had no guts."
+    id: "08xHQCdx5Ed",
+    joke: "Why didn’t the skeleton cross the road? Because he had no guts.",
   },
 ];
 
